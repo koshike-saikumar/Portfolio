ae88ba9a7f45731f6bcba25000ffe69e605766a4
koshike-saikumar <koshikesai25@gmail.com>
2025-06-17 19:46:50 +0530

    Initial commit

---
+++ src/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+    title: "Who I am",
+    description1: "I’m Saikumar, a full-stack web developer with 2.5 years of experience in Java Spring Boot and React.js.",
+    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    image: 2
+}
